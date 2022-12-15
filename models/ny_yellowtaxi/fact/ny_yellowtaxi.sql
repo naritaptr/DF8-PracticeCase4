f77697cb44b5b163efc62acf20dffd34430cc855
naritaptr <91821687+naritaptr@users.noreply.github.com>
2022-12-16 04:51:08 +0700

    Add files via upload

---
+++ models/ny_yellowtaxi/fact/ny_yellowtaxi.sql
@@ -0,0 +1,24 @@
+SELECT * FROM `df-8-371601.ny_yellowtaxi.yellow_tripdata_2019-01`
+UNION ALL
+SELECT * FROM `df-8-371601.ny_yellowtaxi.yellow_tripdata_2019-02`
+UNION ALL
+SELECT * FROM `df-8-371601.ny_yellowtaxi.yellow_tripdata_2019-03`
+UNION ALL
+SELECT * FROM `df-8-371601.ny_yellowtaxi.yellow_tripdata_2019-04`
+UNION ALL
+SELECT * FROM `df-8-371601.ny_yellowtaxi.yellow_tripdata_2019-05`
+UNION ALL
+SELECT * FROM `df-8-371601.ny_yellowtaxi.yellow_tripdata_2019-06`
+UNION ALL
+SELECT * FROM `df-8-371601.ny_yellowtaxi.yellow_tripdata_2019-07`
+UNION ALL
+SELECT * FROM `df-8-371601.ny_yellowtaxi.yellow_tripdata_2019-08`
+UNION ALL
+SELECT * FROM `df-8-371601.ny_yellowtaxi.yellow_tripdata_2019-09`
+UNION ALL
+SELECT * FROM `df-8-371601.ny_yellowtaxi.yellow_tripdata_2019-10`
+UNION ALL
+SELECT * FROM `df-8-371601.ny_yellowtaxi.yellow_tripdata_2019-11`
+UNION ALL
+SELECT * FROM `df-8-371601.ny_yellowtaxi.yellow_tripdata_2019-12`
+ORDER BY lpep_pickup_datetime ASC
---
+++ models/ny_yellowtaxi/fact_trip.sql
@@ -0,0 +1,3 @@
+SELECT  VendorID, RatecodeID, payment_type, trip_distance, tpep_pickup_datetime, tpep_dropoff_datetime, PULocationID,
+        DOLocationID, passenger_count, tpep_pickup_datetime
+FROM `df-8-371601.ny_yellowtaxi`
